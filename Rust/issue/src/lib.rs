747fa2178e915fa89fdb71725cda5492c381233f
mk3250d07295y@aol.com <Yoika>
2022-12-18 18:07:01 +0900

    fix rust program

--- Rust/issue/src/lib.rs
+++ Rust/issue/src/lib.rs
@@ -18,29 +18,18 @@ pub fn frob() {
 }
 
 pub fn auth_token() {
-    println!("Enter the 「secret key」");
-    let mut secret_key = String::new();
-    io::stdin().read_line(&mut secret_key).expect("Failed to read_line.");
-    let trim_secret_key = secret_key.trim().to_string();
+    let secret_key = ask(String::from("Enter the 「secret key」"));
+    let api_key = ask(String::from("Enter the 「api key」"));
+    let frob = ask(String::from("Enter the 「frob」"));
 
-    println!("Enter the 「api key」");
-    let mut api_key = String::new();
-    io::stdin().read_line(&mut api_key).expect("Failed to read_line.");
-    let trim_api_key = api_key.trim().to_string();
-
-    println!("Enter the 「frob」");
-    let mut frob = String::new();
-    io::stdin().read_line(&mut frob).expect("Failed to read_line.");
-    let trim_frob = frob.trim().to_string();
-
-    let code = trim_secret_key + "api_key" + &trim_api_key + "format" + "json" + "frob" + &trim_frob + "method" + "rtm.auth.getToken";
+    let code = secret_key + "api_key" + &api_key + "format" + "json" + "frob" + &frob + "method" + "rtm.auth.getToken";
     let mut md5 = Md5::new();
     md5.input(code.as_bytes());
     println!(
         "{}?api_key={}&format=json&frob={}&method=rtm.auth.getToken&api_sig={}",
         AUTH_TOKEN_URL,
-        trim_api_key,
-        trim_frob,
+        api_key,
+        frob,
         md5.result_str()
     );
 }
